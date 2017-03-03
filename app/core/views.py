a025063ed86fb91922e3c8666b7a59b259f95901
tnummy@gmail.com <tnummy@gmail.com>
2017-03-03 03:44:00 -0500

    final touches

--- app/core/views.py
+++ app/core/views.py
@@ -17,11 +17,18 @@ def index():
 
 
 
-@mod.route('/post')
+@mod.route('/post', methods=['POST'])
 def post():
+  if request.method == 'POST':
+    field_data = request.form
+    payloadData = 'Prop2004:eyJQcm9wNCI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOnRydWUsIm9yZGVyIjoyfSwiUHJvcDYiOnsiZW5hYmxlZCI6dHJ1ZSwibWFuZGF0b3J5Ijp0cnVlLCJvcmRlciI6MX0sIlByb3AxMSI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOmZhbHNlLCJvcmRlciI6OH0sIlByb3AxNSI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOnRydWUsIm9yZGVyIjowfSwiUHJvcDE2Ijp7ImVuYWJsZWQiOnRydWUsIm1hbmRhdG9yeSI6ZmFsc2UsIm9yZGVyIjo0fSwiUHJvcDE3Ijp7ImVuYWJsZWQiOnRydWUsIm1hbmRhdG9yeSI6dHJ1ZSwib3JkZXIiOjN9LCJQcm9wOTciOnsiZW5hYmxlZCI6dHJ1ZSwibWFuZGF0b3J5Ijp0cnVlLCJvcmRlciI6MTB9LCJQcm9wMTEyIjp7ImVuYWJsZWQiOnRydWUsIm1hbmRhdG9yeSI6ZmFsc2UsIm9yZGVyIjo2fSwiUHJvcDExMyI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOmZhbHNlLCJvcmRlciI6N30sIlJlc3VtZSI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOnRydWUsIm9yZGVyIjo1fX0=;'
+    for name, data in field_data.items():
+      if data and (name != 'Prop299' and name != 'Prop300'):
+        payloadData += name + ':' + data + ';'
+
     ws = Webservices()
     # ws.postReq()
-    return (render_template('core/index.html', req=ws.postReq()))
+    return (render_template('core/posted.html', req=ws.postReq(payloadData)))
 
 
 
--- app/core/webservices.py
+++ app/core/webservices.py
@@ -9,12 +9,13 @@ from collections import defaultdict
 class Webservices(object):
 
     def __init__(self):
-        # self.client = 'playground'
-        # self.password = 'PZWcKUPyn'
-        # self.baseurl = "https://ws-" + self.client + ".luceosolutions.com/rest/"
-        #
+        self.client = 'playground'
+        self.password = 'PZWcKUPyn'
+
         self.client = 'kfix'
         self.password = 'zZrnteUtu'
+
+        
         self.baseurl = "https://ws-" + self.client + ".luceosolutions.com/rest/"
 
     def getReqForm(self):
@@ -46,7 +47,8 @@ class Webservices(object):
             else:
                 field_type = ['text']
 
-            propDict.append(flatten([p.xpath('./td[1]/text()'), p.xpath('./td[2]/text()'), field_type, url]))
+            if p.xpath('./td[1]/text()') != ['Prop299'] and p.xpath('./td[1]/text()') != ['Prop300'] and p.xpath('./td[1]/text()') != []:
+                propDict.append(flatten([p.xpath('./td[1]/text()'), p.xpath('./td[2]/text()'), field_type, url]))
 
         for p in propDict:
             if len(p) == 4:
@@ -60,13 +62,17 @@ class Webservices(object):
                             # print key, value
                             plist.append({'id': piece['id'], 'lib': piece['lib']})
                     p.append(plist)
+                elif int(response['recordCount']) == 0:
+                    continue
                 else:
                     p.append([{'id': response['result']['item']['id'], 'lib': response['result']['item']['lib']}])
 
         return propDict
 
 
-    def postReq(self):
+    def postReq(self, payloadData):
         url = 'https://ws-' + self.client + '.luceosolutions.com/rest/position/'
-        payload = {'params': 'Prop11:1;Prop27:90293;Prop28:78558;Prop29:78617;Prop34:90998;Prop35:90995;Prop36:91010;Prop38:Text example;Prop39:1;Prop40:2017-03-03 05:45:46;Prop42:5;Prop43:16;Prop44:Text example;Prop45:Text example;Prop66:Text example;Prop67:Text example;Prop88:Text example;Prop89:Text example;Prop96:Text example;Prop97:Text example;Prop143:38;Prop156:22;Prop157:51;Prop163:49;Prop169:40,48,69;Prop170:40;Prop171:40;Prop172:40;Prop174:13;Prop185:553,549,547;Prop299.lang:1;Prop299:1;Prop300.lang:1;Prop300:1;Prop2003:New York City, NY, US;Prop2004: eyJQcm9wNCI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOnRydWUsIm9yZGVyIjoyfSwiUHJvcDYiOnsiZW5hYmxlZCI6dHJ1ZSwibWFuZGF0b3J5Ijp0cnVlLCJvcmRlciI6MX0sIlByb3AxMSI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOmZhbHNlLCJvcmRlciI6OH0sIlByb3AxNSI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOnRydWUsIm9yZGVyIjowfSwiUHJvcDE2Ijp7ImVuYWJsZWQiOnRydWUsIm1hbmRhdG9yeSI6ZmFsc2UsIm9yZGVyIjo0fSwiUHJvcDE3Ijp7ImVuYWJsZWQiOnRydWUsIm1hbmRhdG9yeSI6dHJ1ZSwib3JkZXIiOjN9LCJQcm9wOTciOnsiZW5hYmxlZCI6dHJ1ZSwibWFuZGF0b3J5Ijp0cnVlLCJvcmRlciI6MTB9LCJQcm9wMTEyIjp7ImVuYWJsZWQiOnRydWUsIm1hbmRhdG9yeSI6ZmFsc2UsIm9yZGVyIjo2fSwiUHJvcDExMyI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOmZhbHNlLCJvcmRlciI6N30sIlJlc3VtZSI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOnRydWUsIm9yZGVyIjo1fX0=;Prop2006:DR32;Prop2010:Text example;Prop2011:Text example;Prop2012:Text example'}
+        # payload = {'params': 'Prop11:1;Prop27:90293;Prop28:78558;Prop29:78617;Prop34:90998;Prop35:90995;Prop36:91010;Prop38:Text example;Prop39:1;Prop40:2017-03-03 05:45:46;Prop42:5;Prop43:16;Prop44:Text example;Prop45:Text example;Prop66:Text example;Prop67:Text example;Prop88:Text example;Prop89:Text example;Prop96:Text example;Prop97:Text example;Prop143:38;Prop156:22;Prop157:51;Prop163:49;Prop169:40,48,69;Prop170:40;Prop171:40;Prop172:40;Prop174:13;Prop185:553,549,547;Prop299.lang:1;Prop299:1;Prop300.lang:1;Prop300:1;Prop2003:New York City, NY, US;Prop2004: eyJQcm9wNCI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOnRydWUsIm9yZGVyIjoyfSwiUHJvcDYiOnsiZW5hYmxlZCI6dHJ1ZSwibWFuZGF0b3J5Ijp0cnVlLCJvcmRlciI6MX0sIlByb3AxMSI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOmZhbHNlLCJvcmRlciI6OH0sIlByb3AxNSI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOnRydWUsIm9yZGVyIjowfSwiUHJvcDE2Ijp7ImVuYWJsZWQiOnRydWUsIm1hbmRhdG9yeSI6ZmFsc2UsIm9yZGVyIjo0fSwiUHJvcDE3Ijp7ImVuYWJsZWQiOnRydWUsIm1hbmRhdG9yeSI6dHJ1ZSwib3JkZXIiOjN9LCJQcm9wOTciOnsiZW5hYmxlZCI6dHJ1ZSwibWFuZGF0b3J5Ijp0cnVlLCJvcmRlciI6MTB9LCJQcm9wMTEyIjp7ImVuYWJsZWQiOnRydWUsIm1hbmRhdG9yeSI6ZmFsc2UsIm9yZGVyIjo2fSwiUHJvcDExMyI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOmZhbHNlLCJvcmRlciI6N30sIlJlc3VtZSI6eyJlbmFibGVkIjp0cnVlLCJtYW5kYXRvcnkiOnRydWUsIm9yZGVyIjo1fX0=;Prop2006:DR32;Prop2010:Text example;Prop2011:Text example;Prop2012:Text example'}
+        print payloadData
+        payload = payloadData
         return requests.post(url, data=payload, auth=(self.client, self.password))
